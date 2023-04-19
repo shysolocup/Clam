211cb28067c3802fd1e59cc76252410ba6846f7e
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-19 10:17:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const { Client, ActivityType } = require('discord.js');
+const bot = new Client({ intents: 33283 });
+const { PSClient } = require('discordpps');
+const psc = new PSClient({ client: bot, prefix: "!" });
 
+const config = require('./config/config.json');
+
+bot.login(config.token);
