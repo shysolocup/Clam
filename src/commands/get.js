d9dc16bad9e36cfe6ee556494583237e894eb334
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-20 08:23:05 -0500

    Update get.js

--- src/commands/get.js
+++ src/commands/get.js
@@ -1,1 +1,12 @@
+var { psc, bot } = require('../../index.js');
+var { colors, acceptEmoji } = require('../assets');
+var { Clanner } = require('../classes');
 
+async function data(ctx, cmd) {
+	let clans = new Clanner();
+	let id = cmd.args[0];
+
+	console.log(clans.fetch(id, ctx.guild.id));
+}
+
+psc.command("get", data);
