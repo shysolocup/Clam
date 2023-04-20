2307889079e51431e33a72f7365d66e4191433a0
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-20 08:11:23 -0500

    Update stuff.js

--- src/stuff.js
+++ src/stuff.js
@@ -1,1 +1,16 @@
+const { random } = require('stews');
 
+class ID {
+    constructor() {
+        let id = [];
+		let chars = "abcdefghijklmnopqrstuvwxyz1234567890".split("");
+
+		for (let i = 0; i < 4; i++) {
+			id.push(random.choice(chars));
+		}
+
+        return id;
+    }
+}
+
+module.exports = { ID };
