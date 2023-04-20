ebd95e27265662977cd8153d8d229d76c4043ce9
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-20 08:19:57 -0500

    Create Clan.js

---
+++ src/classes/Clan.js
@@ -0,0 +1,38 @@
+const { Soup } = require('stews');
+const { ClanTemplate } = require('../data/template.js');
+const { ID } = require('../stuff.js');
+
+class Clan {
+    constructor(ctx) {
+        var clans = Soup.from(require('../data/clans.json'));
+        var id = new ID();
+
+        var contents = new ClanTemplate(ctx, id);
+
+        if (!clans.has(ctx.guild.id)) clans.push(ctx.guild.id, new Soup(Object));
+        var parent = Soup.from(clans.get(ctx.guild.id));
+
+        if (!parent.has(contents.id)) parent.push(contents.id, contents);
+        
+        clans.dump('./src/data/clans.json', null, 4);
+
+        contents.forEach( (key, value) => {
+            this[key] = value;
+        });
+
+        return new Proxy(this, ClanProxyHandler());
+    }
+}
+
+function ClanProxyHandler() {
+    return {
+        get(target, prop) {
+            return target[prop];
+        },
+		set(target, prop, value) {
+			return target[prop] = value;
+		}
+    }
+}
+
+module.exports = { Clan };
