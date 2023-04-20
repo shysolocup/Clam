19875844140f04ed6501dbc59683c8f52c59fbb5
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-20 08:22:21 -0500

    Update create.js

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,1 +1,19 @@
+var { psc, bot } = require('../../index.js');
+var { colors, acceptEmoji } = require('../assets');
 
+async function data(ctx, cmd) {
+	const { Clan } = require('../classes');
+
+	let clan = new Clan(ctx);
+	
+	const embed = new psc.Embed({
+		title: "Clan Creation :sparkles:",
+		description: `${acceptEmoji} Created your new clan!`,
+		footer: `( id: ${clan.id} )`,
+		color: colors.accept
+	});
+
+	ctx.reply({ embeds: [embed] });
+}
+
+psc.command("create", data);
