3acc6bbf93d978bc190af764743fff3db690bfd2
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-20 08:08:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 const { Client, ActivityType } = require('discord.js');
 const bot = new Client({ intents: 33283 });
-const { PSClient } = require('discordpps');
+const { PSClient } = require('./src/packages/discordpps');
 const psc = new PSClient({ client: bot, prefix: "!" });
 
 const config = require('./config/config.json');
+const { Clanner } = require('./src/classes');
+const clans = new Clanner();
 
+/* login stuff */
 bot.on("ready", () => {
 	console.log(`Logged in as ${bot.user.tag}`);
-	bot.user.setPresence({
-		activities: [{
-			name: `over <count would go here but I haven't added it yet> clans`,
-			type: ActivityType.Watching
-		}]
-	});
+	update();
 });
 
 
+/* status updates on message */
+bot.on("messageCreate", () => {
+	update();
+});
+
+
+/* status updater */
+function update() {
+	bot.user.setPresence({
+		activities: [{
+			name: `over ${clans.count()} ${(clans.count() == 1) ? "clan" : "clans" }`,
+			type: ActivityType.Watching
+		}]
+	});
+}
+
+
+/* logs in and sets up the stuff for the commands to work */
 bot.login(config.token);
+module.exports = { psc, bot };
+
+
+/* builds all the commands */
+require('./src/commands/build'); 
