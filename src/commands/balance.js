84fcc96debca461a9b7e1e9f0bf971fd9f217759
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-25 18:08:12 -0500

    Update balance.js

--- src/commands/balance.js
+++ src/commands/balance.js
@@ -2,11 +2,11 @@ var { psc, bot } = require('../../index.js');
 var { colors, colorify, pearl, pearlify, declineEmoji } = require('../assets');
 
 async function data(ctx, cmd) {
-	const { Hand, Econner } = require('../classes');
+	const { Econner } = require('../classes');
 	let econner = new Econner();
 
 	
-	let user = (cmd.args[0]) ? psc.fetchUser(cmd.args[0]) : ctx.author;
+	let user = (cmd.args[0]) ? await psc.fetchUser(cmd.args[0]) : ctx.author;
 
 	
 	/* handling */
@@ -20,19 +20,19 @@ async function data(ctx, cmd) {
 	}
 
 
-	/* setup stuff */
-	if (!econner.has(user.id)) new Hand(user.id);
-	let balance = econner.fetchHand(user.id);
+	let balance = (econner.has(user.id)) ? econner.fetchHand(user.id) : 0;
 
 
 	/* field stuff */
 	let hand = `${pearl}${ (econner.has(user.id)) ? pearlify(Math.round(balance)) : 0 }`;
 	let rank = colorify( (econner.has(user.id)) ? pearlify(Math.round(balance)) : 0 )[1];
 
+	let username = user.username.split("");username[0]=username[0].toUpperCase();username=username.join("");
+
 	
 	/* embed stuff */
 	let embed = new psc.Embed({
-		title: `${ (ctx.author.id == user.id) ? "Your" : `${user.username}'s` } Balance`,
+		title: `${ (ctx.author.id == user.id) ? "Your" : `${username}'s` } Balance  :bucket:`,
 		description: (ctx.author.id == user.id) ? (balance <= -999999999999999) ? "How did you manage this" : (balance < 0) ? "(PS. Enjoy rank 5 poor)" : "" : null,
 
 		fields: [
@@ -40,6 +40,7 @@ async function data(ctx, cmd) {
 			{ name: "Rank", value: "`" + `${rank}` + "`", inline: true }
 		],
 
+		footer: { text: user.tag, icon_url: psc.user.avatar(user) },
 		color: colorify(balance)[0]
 	});
 
