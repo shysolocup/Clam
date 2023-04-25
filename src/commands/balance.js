4431c2c974cba81c58c8850c0568742713b27a2d
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-25 10:36:24 -0500

    Update balance.js

--- src/commands/balance.js
+++ src/commands/balance.js
@@ -1,1 +1,36 @@
+var { psc, bot } = require('../../index.js');
+var { colorify, pearl, pearlify } = require('../assets');
 
+async function data(ctx, cmd) {
+	const { Hand, Econner } = require('../classes');
+	let econner = new Econner();
+
+	let user = (cmd.args[0]) ? psc.fetchUser(cmd.args[0]) : ctx.author;
+
+	if (!econner.has(user.id)) new Hand(user.id);
+
+	let balance = econner.fetchHand(user.id);
+
+
+	/* field stuff */
+	let hand = `${pearl}${ (econner.has(user.id)) ? pearlify(Math.round(balance)) : 0 }`;
+	let rank = colorify( (econner.has(user.id)) ? pearlify(Math.round(balance)) : 0 )[1];
+
+	
+	/* embed stuff */
+	let embed = new psc.Embed({
+		title: `${ (ctx.author.id == user.id) ? "Your" : `${user.username}'s` } Balance`,
+		description: (ctx.author.id == user.id) ? (balance <= -999999999999999) ? "How did you manage this" : (balance < 0) ? "(PS. Enjoy rank 5 poor)" : "" : null,
+
+		fields: [
+			{ name: "Hand", value: "`" + `${hand}` + "`", inline: true },
+			{ name: "Rank", value: "`" + `${rank}` + "`", inline: true }
+		],
+
+		color: colorify(balance)[0]
+	});
+
+	ctx.reply({ embeds: [embed] });
+}
+
+psc.command({name: "balance", aliases: ["bal"]}, data);
