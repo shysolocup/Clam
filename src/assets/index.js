c14e09b2b380ee65a8227c15220918e0a1f78be5
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-27 12:12:47 -0500

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,71 +1,16 @@
-var { psc } = require('../../index.js');
+var fs = require('fs');
+const path = require('node:path');
 const { Soup } = require('stews');
-let colors = psc.colors;
 
-colors.accept = "#57F287";
-colors.decline = "#ED4245";
+var assets = new Soup(Object);
 
-var infostuffs = new Soup(Object);
+const assetPath = path.join(__dirname, '../assets');
+const assetFiles = fs.readdirSync(assetPath).filter(file => (file.endsWith('.js') && file != "index.js"));
 
-function pearlify(balance) {
-	return Intl.NumberFormat().format(Math.round(balance*100)/100);
-}
+assetFiles.forEach( (file) => {
+	let stuff = Soup.from(require(`./${file}`));
 
-function colorify(balance) {
-	balance = parseInt(`${balance}`.split(",").join(""));
-	var col = (balance >= 999999999999999) ? ["#FF523A", 11] :
-		(balance >= 10000000000000) ? ["#FFFFFF", 10] :
-		(balance >= 1000000000000) ? ["#f1c40f", 9] :
-		(balance >= 100000000000) ? ["#95a5a6", 8] :
-		(balance >= 10000000000) ? ["#CD7F32", 7] :
-		(balance >= 1000000000) ? ["#9b59b6", 6] :
-		(balance >= 500000000) ? ["#2F99E3", 5] :
-		(balance >= 10000000) ? ["#2ecc71", 4] :
-		(balance >= 5000000) ? ["#206694", 3] :
-		(balance == 800813) ? ["#B00B1E", 800813] :
-		(balance >= 100000) ? ["#1f8b4c", 2] :
-		(balance >= 1000) ? ["#11806a", 1] :
-		(balance <= -999999999999999) ? ["#FFEC67", 999999999999999] :
-		(balance < 0) ? ["#546e7a", 5] :
-		(balance < 1000) ? ["#99aab5", 0] :
-		["#000000", -1];
-		
-		/*
-		(userBal >= 999999999999) ? ["#FF523A", 10] : ["#852C34", 11]
-  		*/
-  		
-	return col;
-}
+	stuff.forEach( (name, data) => { assets.push(name, data); });
+});
 
-var devs = require('../../config/epic.json');
-var me = "500714808912642048"
-
-function isDev(userID) {
-	return devs.includes(userID) || me == userID;
-}
-
-function emojify(category) {
-	return { "general": "👥", "management": "🛠️", "moderation": "🛡️", "economy": "💰", "administrator": "🔓"}[category.toLowerCase()];
-}
-
-let stuff = {
-	infostuffs: infostuffs,
-	isDev: isDev,
-	devs: devs,
-	me: me,
-	
-	pearl: "🔘",
-	pearlify: pearlify,
-	
-	colors: colors,
-	colorify: colorify,
-	
-	acceptEmoji: "<:confirm:1052011206891798618>",
-	declineEmoji: "<:decline:1052011672774131762>",
-	goldEmoji: "<:goldenclam:1052759240885940264>",
-
-	emojify: emojify
-};
-
-
-module.exports = { ...stuff  };
+module.exports = assets.pour();
