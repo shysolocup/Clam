71d3fea8556f1aa88f4efd68b0c05e708aaaa45e
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-27 14:34:15 -0500

    Update Catch.js

--- src/classes/Catch.js
+++ src/classes/Catch.js
@@ -1,36 +1,33 @@
 const { Embed } = require('../packages/discordpps');
 
-class Catch {
-    constructor(call, contents={ head:null, text:null, footer:null, color:null, emoji:null, fields:null, author:null, time:null }) {
+function Catch(call, contents={ head:null, text:null, footer:null, color:null, emoji:null, fields:null, author:null, time:null }) {
+    var { colors, emojis } = require('../assets');
+    var { psc } = require('../../index.js');
 
-        var { colors, emojis } = require('../assets');
-        var { psc } = require('../../index.js');
+    let emoji = (contents.emoji) ? contents.emoji : (call) ? emojis.decline : emojis.accept;
+    let color = (contents.color) ? contents.color : (call) ? colors.decline : colors.accept;
+    let time = (contents.time) ? contents.time : "3s";
+    
+    if (call) psc.reply({
+        embeds: [
+            new Embed({
+                title: contents.head,
+                description: `${emoji}  ${contents.text}`,
 
-        let emoji = (contents.emoji) ? contents.emoji : (call) ? emojis.decline : emojis.accept;
-        let color = (contents.color) ? contents.color : (call) ? colors.decline : colors.accept;
-        let time = (contents.time) ? contents.time : "3s";
-        
-        if (call) psc.reply({
-            embeds: [
-                new Embed({
-                    title: contents.head,
-                    description: `${emoji}  ${contents.text}`,
+                fields: contents.fields,
 
-                    fields: contents.fields,
+                author: contents.author,
+                footer: contents.footer,
 
-                    author: contents.author,
-                    footer: contents.footer,
+                ephemeral: true,
+                color: color
+            })
+        ],
 
-                    ephemeral: true,
-                    color: color
-                })
-            ],
+        deleteAfter: time
+    })
 
-            deleteAfter: time
-        })
-
-        return call;
-    }
+    return call;
 }
 
 module.exports = { Catch };
