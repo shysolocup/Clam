b7ce79cec01a249460420f67630645b0a9b6c317
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-27 13:51:23 -0500

    Update Handle.js

--- src/classes/Handle.js
+++ src/classes/Handle.js
@@ -1,11 +1,17 @@
 const { Embed } = require('../packages/discordpps');
 
 class Handle {
-    constructor(type, contents={ head:null, text:null, footer:null, color:null, emoji:null, fields:null, author:null, time:null }) {
+    constructor(bool, handles = { 
+            pass: { head:null, text:null, footer:null, color:null, emoji:null, fields:null, author:null, time:null },
+            fail: { head:null, text:null, footer:null, color:null, emoji:null, fields:null, author:null, time:null }
+        }) {
+
         var { colors, emojis } = require('../assets');
-        
-        let emoji = (contents.emoji) ? contents.emoji : (type==0) ? emojis.fail : emojis.success;
-        let color = (contents.color) ? contents.color : (type==0) ? colors.fail : colors.success;
+
+        var contents = (bool) ? handles.pass : handles.fail;
+
+        let emoji = (contents.emoji) ? contents.emoji : (bool) ? emojis.accept : emojis.decline;
+        let color = (contents.color) ? contents.color : (bool) ? colors.accept : colors.decline;
         let time = (contents.time) ? contents.time : "3s";
         
         return {
