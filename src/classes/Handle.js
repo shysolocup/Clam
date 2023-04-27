b1e6177bbd130b4ca10f626cc8da05754d43ce9d
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-27 13:16:30 -0500

    Update Handle.js

--- src/classes/Handle.js
+++ src/classes/Handle.js
@@ -2,22 +2,24 @@ const { Embed } = require('../packages/discordpps');
 var { colors, emojis } = require('../assets');
 
 class Handle {
-    constructor(type, contents={ title:null, text:null, footer:null, color:null, emoji:null, fields:null, author:null }) {
+    constructor(type, contents={ head:null, text:null, footer:null, color:null, emoji:null, fields:null, author:null }) {
         let emoji = (contents.emoji) ? contents.emoji : (type==0) ? emojis.fail : emojis.success;
         let color = (contents.color) ? contents.color : (type==0) ? colors.fail : colors.success;
         
-        return new Embed({
-            title: contents.title,
-            description: `${emoji}  ${contents.text}`,
+        return [
+            new Embed({
+                title: contents.head,
+                description: `${emoji}  ${contents.text}`,
 
-            fields: contents.fields,
+                fields: contents.fields,
 
-            author: contents.author,
-            footer: contents.footer,
+                author: contents.author,
+                footer: contents.footer,
 
-            ephemeral: true,
-            color: color
-        });
+                ephemeral: true,
+                color: color
+            }) 
+        ];
     }
 }
 
