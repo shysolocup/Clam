60506a374d3fcb8eb1a5fda4854e00a68b5486a8
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-27 16:29:48 -0500

    Update Catch.js

--- src/classes/Catch.js
+++ src/classes/Catch.js
@@ -1,14 +1,15 @@
-=const { Embed } = require('../packages/discordpps');
+const { Embed } = require('../packages/discordpps');
 
-function Catch(call, contents={ head:null, text:null, footer:null, color:null, emoji:null, fields:null, author:null, time:null }) {
+function Catch(call, contents={ head:null, text:null, footer:null, color:null, emoji:null, fields:null, author:null, time:null, delete:true, poster:null }) {
     var { colors, emojis } = require('../assets');
     var { psc } = require('../../index.js');
 
     let emoji = (contents.emoji) ? contents.emoji : (call) ? emojis.fail : emojis.success;
     let color = (contents.color) ? contents.color : (call) ? colors.fail : colors.success;
-    let time = (contents.time != false) ? contents.time : (contents.time == false) ? null : "3s";
+    let time = (contents.time && contents.delete) ? contents.time : (contents.delete == false) ? null : "3s";
+	let poster = (contents.poster) ? contents.poster : psc.reply;
     
-    if (call) psc.reply({
+    if (call) poster({
         embeds: [
             new Embed({
                 title: contents.head,
