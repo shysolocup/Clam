baa4b6fdf88b6c500c3be5cf944ea2ac0748c38f
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-27 15:09:03 -0500

    Update Catch.js

--- src/classes/Catch.js
+++ src/classes/Catch.js
@@ -4,8 +4,8 @@ function Catch(call, contents={ head:null, text:null, footer:null, color:null, e
     var { colors, emojis } = require('../assets');
     var { psc } = require('../../index.js');
 
-    let emoji = (contents.emoji) ? contents.emoji : (call) ? emojis.decline : emojis.accept;
-    let color = (contents.color) ? contents.color : (call) ? colors.decline : colors.accept;
+    let emoji = (contents.emoji) ? contents.emoji : (call) ? emojis.fail : emojis.success;
+    let color = (contents.color) ? contents.color : (call) ? colors.fail : colors.success;
     let time = (contents.time) ? contents.time : "3s";
     
     if (call) psc.reply({
