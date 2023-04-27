23df3c69c22d12637281e2f6064965184084f811
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-04-27 11:53:57 -0500

    Update emojis and emojify.js

--- src/assets/emojis and emojify.js
+++ src/assets/emojis and emojify.js
@@ -1,9 +1,11 @@
-let acceptEmoji = "<:confirm:1052011206891798618>";
-let declineEmoji = "<:decline:1052011672774131762>";
-let goldEmoji = "<:goldenclam:1052759240885940264>";
+let emojis = {
+	accept: "<:confirm:1052011206891798618>",
+	decline: "<:decline:1052011672774131762>",
+	gold: "<:goldenclam:1052759240885940264>"
+};
 
 function emojify(category) {
 	return { "general": "👥", "management": "🛠️", "moderation": "🛡️", "economy": "💰", "administrator": "🔓"}[category.toLowerCase()];
 }
 
-module.exports = { acceptEmoji, declineEmoji, goldEmoji, emojify };
+module.exports = { emojis emojify };
