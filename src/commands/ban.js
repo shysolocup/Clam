5380979d07e4740ae6ea1b03aef1397982e967cd
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-01 14:34:54 -0500

    Update ban.js

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -34,7 +34,8 @@ async function data(ctx, cmd) {
 	if (
 		Catch( !clan.ops.includes(ctx.author.id) && clan.owner != ctx.author.id, { text: "You have to have operator to ban users."}) ||
 		Catch( clan.ops.includes(user.id) && clan.owner != ctx.author.id, { text: "Only the owner can ban operators."}) ||
-		Catch( clan.bans.includes(user.id), { text: 'User is already banned from that clan.'})
+		Catch( clan.bans.includes(user.id), { text: 'User is already banned from that clan.'}) ||
+		Catch( clan.owner == user.id, { text: "You can't ban the owner of the clan."})
 	) return;
 
 	
