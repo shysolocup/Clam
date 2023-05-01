c566968ecd3c0541e39d27e841cf10f2091f428f
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-01 11:21:05 -0500

    Create listall.js

---
+++ src/commands/listall.js
@@ -0,0 +1,69 @@
+var { psc, bot } = require('../../index.js');
+var { colors, emojis, infostuffs } = require('../assets');
+var { Clanner, Catch } = require('../classes');
+
+const { Soup } = require('stews');
+
+async function data(ctx, cmd) {
+	var clans = new Clanner();
+	var list = clans.listify(ctx.guild.id, true);
+    var disabled = list.total <= 0;
+
+	var page = 0;
+	
+	
+	/* buttons */
+	let bigLeftButton = new psc.Button({ id: "clanAll/bigLeft", emoji: "⏮️", style: "secondary", disabled: disabled });
+	let leftButton = new psc.Button({ id: "clanAll/left", emoji: "◀", style: "secondary", disabled: disabled });
+	let rightButton = new psc.Button({ id: "clanALl/right", emoji: "▶", style: "secondary", disabled: disabled });
+    let bigRightButton = new psc.Button({ id: "clanAll/bigRight", emoji: "⏭️", style: "secondary", disabled: disabled });
+	
+	let row = new psc.ActionRow([ bigLeftButton, leftButton, rightButton, bigRightButton ]);
+
+
+	/* catch and embed stuff */
+	if ( 
+		Catch( !clans.all[ctx.guild.id], {
+			head: `All Clans in ${ctx.guild.name}`,
+			text: `There are no clans in this server`,
+
+			fields: [{
+				name: "• None", 
+				value: "** ** Use !create to start up your own clan!"
+			}],
+
+			color: colors.clam,
+
+			thumbnail: ctx.guild.iconURL(),
+			footer: `Page 1/1`,
+			
+			delete: false,
+			textEmoji: false,
+			components: [row]
+		}) 
+	) return;
+	
+
+
+    /* normal embed stuff */
+	let embed = new psc.Embed({
+        title: `All Clans in ${ctx.guild.name}`,
+		description: `There ${ (list.total == 1) ? "is" : "are" } ${list.total} ${ (list.total == 1) ? "clan" : "clans" } in this server`,
+
+		fields: list.fields[page],
+
+		color: colors.clam,
+
+        thumbnail: ctx.guild.iconURL(),
+		footer: `Page ${page+1}/${list.pages}`
+    });
+
+	
+	let a = await ctx.reply({ embeds: [embed], components: [row] } ).catch(e=>{});
+	
+
+    infostuffs.push(a.id, [ctx.author, page]);
+	setTimeout(() => infostuffs.delete(a.id), 21600000);
+}
+
+psc.command({ name: "listall", aliases: ["viewall"] }, data);
