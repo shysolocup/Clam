6f7a6fc4562bfa51e11cd1eb4734c6b389fb85d4
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-01 11:09:25 -0500

    Update Catch.js

--- src/classes/Catch.js
+++ src/classes/Catch.js
@@ -1,19 +1,25 @@
 const { Embed } = require('../packages/discordpps');
 
-function Catch(call, contents={ head:null, text:null, footer:null, color:null, emoji:null, fields:null, author:null, time:null, delete:true, poster:null, components:null, files:null, image:null, thumbnail:null }) {
+function Catch(call, contents={ head:null, text:null, footer:null, color:null, emoji:null, fields:null, author:null, time:null, delete:true, poster:null, components:null, files:null, image:null, thumbnail:null, textEmoji:true }) {
     var { colors, emojis } = require('../assets');
     var { psc } = require('../../index.js');
+
+
+    contents.delete = (contents.delete != undefined) ? contents.delete : true;
+    contents.textEmoji = (contents.textEmoji != undefined) ? contents.textEmoji : true;
+
 
     let emoji = (contents.emoji) ? contents.emoji : (call) ? emojis.fail : emojis.success;
     let color = (contents.color) ? contents.color : (call) ? colors.fail : colors.success;
     let time = (contents.time && contents.delete) ? contents.time : (contents.delete == false) ? null : "3s";
 	let poster = (contents.poster) ? contents.poster : psc.reply;
     
+
     if (call) poster({
         embeds: [
             new psc.Embed({
                 title: contents.head,
-                description: (contents.text) ? `${emoji}  ${contents.text}` : undefined,
+                description: (contents.text) ? `${ (contents.textEmoji) ? `${emoji}  ` : ""}${contents.text}` : undefined,
 
                 fields: contents.fields,
 
@@ -34,6 +40,7 @@ function Catch(call, contents={ head:null, text:null, footer:null, color:null, e
         deleteAfter: time
 
     }).catch(e=>{});
+
 
     return call;
 }
