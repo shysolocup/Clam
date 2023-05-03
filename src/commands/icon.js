09420e3a9209d93db8d8875ebd4c1d78db8ef36d
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-03 09:55:38 -0500

    Update icon.js

--- src/commands/icon.js
+++ src/commands/icon.js
@@ -1,1 +1,39 @@
+var { psc, bot } = require('../../index.js');
+var { emojis } = require('../assets');
+var { Clanner, Catch } = require('../classes');
 
+const { Soup } = require('stews');
+
+async function data(ctx, cmd) {
+	let clans = new Clanner();
+	let id = cmd.args[0];
+	
+	
+	/* handling */
+	if (
+		Catch( !id, { text: "Please put a clan ID."}) ||
+		Catch( !clans.has(id, ctx.guild.id), { text: "There is no clan with that ID." })
+		
+	) { return }
+
+
+	/* the stuff */
+	let clan = clans.fetch(id, ctx.guild.id);
+
+	try {
+		var name = (clan.gold) ? `${clan.name}  ${emojis.gold}` : clan.name;
+		
+		var icon = Soup.from(clan.icon).replaceAll(" ", "_").join("");
+		var banner = Soup.from(clan.banner).replaceAll(" ", "_").join("");
+	}
+	catch(err) {
+		return Catch( true, { text: "A required part of the clan has been removed or altered.", poster: ctx.reply });
+    }
+    
+    ctx.reply({
+        content: `Here's the icon for ${clan.name}:`,
+        files: [{ attachment: icon, name: `clan-icon.png` }]
+    })
+}
+
+psc.command("icon", data);
