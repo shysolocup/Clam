4dc3c52cbac19074766b6ee2c3e3e3e66a291d4c
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-03 10:49:17 -0500

    Create Item.js

---
+++ src/classes/Item.js
@@ -0,0 +1,37 @@
+const { Soup } = require('stews');
+const { ItemTemplate } = require('./ItemTemplate.js');
+
+class Item {
+    constructor(ctx, name) {
+        var items = Soup.from(require('../data/items.json'));
+
+        var contents = new ItemTemplate(ctx, name);
+
+        if (!items.has(ctx.guild.id)) items.push(ctx.guild.id, new Soup(Object));
+        var parent = Soup.from(items.get(ctx.guild.id));
+
+        if (!parent.has(contents.name)) parent.push(contents.name, contents),
+        
+        items.dump('./src/data/items.json', null, 4);
+
+        contents.forEach( (key, value) => {
+            this[key] = value;
+        });
+
+        return new Proxy(this, ItemProxyHandler());
+    }
+}
+
+function ItemProxyHandler() {
+    return {
+        get(target, prop) {
+            return target[prop];
+        },
+		set(target, prop, value) {
+			target[prop] = value;
+			return true;
+		}
+    }
+}
+
+module.exports = { Item };
