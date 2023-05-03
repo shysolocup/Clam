ccff97bffe1a33fc3319b38bbb01160303bc41e8
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-03 10:59:32 -0500

    Create ItemTemplate.js

---
+++ src/classes/ItemTemplate.js
@@ -0,0 +1,24 @@
+const { Soup } = require('stews');
+
+class IconTemplate {
+    constructor(ctx, clanID, name=null) {
+        var prefixes = require('../../config/prefixes.json');
+        var prefix = (prefixes instanceof Object && prefixes[ctx.guild.id]) ? prefixes[ctx.guild.id] : (prefixes instanceof Object) ? prefixes.default : prefixes;
+		
+        return new Soup({
+            name: (name) ? name : `New Item`,
+            description: `Use `+"`"+`${prefix}item set`+"`"+` to change things to your liking!`,
+            creator: ctx.author.id,
+            guild: ctx.guild.id,
+            clan: clanID,
+            roles: {
+                add: [],
+                remove: []
+            },
+            price: 0
+        });  
+        
+    }
+}
+
+module.exports = { ItemTemplate };
