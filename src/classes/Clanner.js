a105acafbd6039288eb38007c222e2498071dbe4
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-04 12:08:36 -0500

    Update Clanner.js

--- src/classes/Clanner.js
+++ src/classes/Clanner.js
@@ -46,8 +46,8 @@ class Clanner {
 	}
 
 
-	set(id, name, value) {
-		let clan = this.fetch(id);
+	set(id, name, value, guildID) {
+		let clan = this.fetch(id, guildID);
 		var clans = Soup.from(require('../data/clans.json'));
 
 		clans[clan.guild][id][name] = value;
@@ -55,82 +55,93 @@ class Clanner {
 	}
 
 
-	shout(id, content, author) {
-		let shout = new Shout(content, author);
-		this.set(id, "shout", shout);
+	setID(id, value, guildID) {
+		let clan = this.fetch(id, guildID);
+		var clans = Soup.from(require('../data/clans.json'));
+
+		clans[clan.guild] = Soup.from(clans[clan.guild]);
+		clans[clan.guild][id].id = value;
+		clans[clan.guild].rename(id, value);
+		clans.dump("./src/data/clans.json", null, 4);
 	}
 
 
-	join(id, userID) {
-		let members = Soup.from(this.fetch(id).members);
-		let bans = Soup.from(this.fetch(id).bans);
+	shout(id, content, author, guildID) {
+		let shout = new Shout(content, author);
+		this.set(id, "shout", shout, guildID);
+	}
+
+
+	join(id, userID, guildID) {
+		let members = Soup.from(this.fetch(id, guildID).members);
+		let bans = Soup.from(this.fetch(id, guildID).bans);
 
 		if (!members.has(userID) && !bans.has(userID)) members.push(userID);
 
-		this.set(id, "members", members.pour() );
+		this.set(id, "members", members.pour(), guildID);
 	}
 
 
-	leave(id, userID) {
-		let members = Soup.from(this.fetch(id).members);
-		let ops = Soup.from(this.fetch(id).ops);
-		let owner = this.fetch(id).owner;
+	leave(id, userID, guildID) {
+		let members = Soup.from(this.fetch(id, guildID).members);
+		let ops = Soup.from(this.fetch(id, guildID).ops);
+		let owner = this.fetch(id, guildID).owner;
 
 		if (userID == owner) return;
 		if (members.has(userID)) members.remove(userID);
 		if (ops.has(userID)) ops.remove(userID);
 
-		this.set(id, "members", members.pour());
-		this.set(id, "ops", ops.pour());
+		this.set(id, "members", members.pour(), guildID);
+		this.set(id, "ops", ops.pour(), guildID);
 	}
 
 
-	ban(id, userID) {
-		let bans = Soup.from(this.fetch(id).bans);
-		let ops = Soup.from(this.fetch(id).ops);
-		let owner = this.fetch(id).owner;
+	ban(id, userID, guildID) {
+		let bans = Soup.from(this.fetch(id, guildID).bans);
+		let ops = Soup.from(this.fetch(id, guildID).ops);
+		let owner = this.fetch(id, guildID).owner;
 
 		if (ops.has(userID) || owner == userID) return;
 		if (!bans.has(userID)) {
 
 			bans.push(userID);
-			this.leave(id, userID);
+			this.leave(id, userID, guildID);
 
 		}
 
-		this.set(id, "bans", bans.pour());
+		this.set(id, "bans", bans.pour(), guildID);
 	}
 
 
-	unban(id, userID) {
-		let bans = Soup.from(this.fetch(id).bans);
+	unban(id, userID, guildID) {
+		let bans = Soup.from(this.fetch(id, guildID).bans);
 		if (bans.has(userID)) bans.remove(userID);
 
-		this.set(id, "bans", bans.pour());
+		this.set(id, "bans", bans.pour(), guildID);
 	}
 
 
-	op(id, userID) {
-		let members = Soup.from(this.fetch(id).members);
-		let ops = Soup.from(this.fetch(id).ops);
+	op(id, userID, guildID) {
+		let members = Soup.from(this.fetch(id, guildID).members);
+		let ops = Soup.from(this.fetch(id, guildID).ops);
 
 		if (!members.has(userID)) return;
 		if (!ops.has(userID)) ops.push(userID);
 
-		this.set(id, "ops", ops.pour());
+		this.set(id, "ops", ops.pour(), guildID);
 	}
 
 
-	deop(id, userID) {
-		let ops = Soup.from(this.fetch(id).ops);
+	deop(id, userID, guildiD) {
+		let ops = Soup.from(this.fetch(id, guildID).ops);
 		if (ops.has(userID)) ops.remove(userID);
 
-		this.set(id, "ops", ops.pour());
+		this.set(id, "ops", ops.pour(), guildID);
 	}
 
 
-	transfer(id, userID) {
-		this.set(id, "owner", userID);
+	transfer(id, userID, guildID) {
+		this.set(id, "owner", userID, guildID);
 	}
 
 	
