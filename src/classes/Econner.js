cde49d1fd06456220cab25c43fd10b2293a1e64e
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-05 10:07:40 -0500

    Update Econner.js

--- src/classes/Econner.js
+++ src/classes/Econner.js
@@ -6,6 +6,7 @@ class Econner {
 		return Soup.from(require('../data/economy.json'));
 	}
 
+
     async in(guildID) {
         var hands = (Soup.from(require('../data/economy.json'))).entries;
         let { psc } = require('../../index.js');
@@ -18,51 +19,89 @@ class Econner {
 		return Object.fromEntries(hands.sort( (a, b) => { return b[1] - a[1] }));
     }
 
+
 	fetchHand(id) {
         let hands = Soup.from(require('../data/economy.json'));
         return hands.get(id);
 	}
+
 
     fetchClan(id, guildID=null) {
         let clans = new Clanner();
         return clans.fetch(id, guildID).funds;
     }
 
+
     addHand(amount, id) {
         let hands = Soup.from(require('../data/economy.json'));
         hands.set(id, (amount+hands.get(id)) );
+        
         hands.dump('./src/data/economy.json', null, 4);
     }
+
 
     removeHand(amount, id) {
         let hands = Soup.from(require('../data/economy.json'));
         hands.set(id, (amount-hands.get(id)) );
+        
         hands.dump('./src/data/economy.json', null, 4);
     }
+
 
     setHand(amount, id) {
         let hands = Soup.from(require('../data/economy.json'));
         hands.set(id, amount);
+        
         hands.dump('./src/data/economy.json', null, 4);
     }
 
-    addClan(amount, id) {
+
+    addClan(amount, id, guildID) {
         let clans = new Clanner();
+        let clan = clans.fetch(id, guildID);
+        
+        clans.set(id, "funds", clan.funds+amount, guildID);
     }
+    
+    
+    removeClan(amount, id, guildID) {
+        let clans = new Clanner();
+        let clan = clans.fetch(id, guildID);
+        
+        clans.set(id, "funds", clan.funds-amount, guildID);
+    }
+    
+    
+    setClan(amount, id, guildID) {
+        let clans = new Clanner();
+        let clan = clans.fetch(id, guildID);
+        
+        clans.set(id, "funds", amount, guildID);
+    }
+	
 	
 	has(id) {
 		return Soup.from(require('../data/economy.json')).has(id);
 	}
 
-    deposit(amount, userID, clanID) {
+
+    deposit(amount, userID, clanID, guildID) {
         this.removeHand(amount, userID);
-	    this.addClan(amount, clanID);
+	    this.addClan(amount, clanID, guildID);
     }
 	
-	withdraw(amount, userID, clanID) {
+	
+	withdraw(amount, userID, clanID, guildID) {
 		this.addHand(amount, userID);
-		this.removeClan(amount, clanID);
+		this.removeClan(amount, clanID, guildID);
 	}
+	
+	
+	give(amount, victim, reciever) {
+        this.removeHand(amount, victim);
+	    this.addHand(amount, reciever);
+    }
+	
 	
 	async userLB(guildID=null) {
 		var hands = (Soup.from(require('../data/economy.json'))).entries;
@@ -77,6 +116,7 @@ class Econner {
 		
 		return Object.fromEntries(hands.sort( (a, b) => { return b[1] - a[1] }));
 	}
+	
 	
 	async clanLB(guildID=null) {
 		let clans = Soup.from((new Clanner()).every()).entries;
