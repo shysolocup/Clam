ea2846cb62036a4f20240a095df02ca5d1d4acb0
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-05 10:13:05 -0500

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,12 +5,16 @@ const { Soup } = require('stews');
 var assets = new Soup(Object);
 
 const assetPath = path.join(__dirname, '../assets');
-const assetFiles = fs.readdirSync(assetPath).filter(file => (file.endsWith('.js') && file != "index.js"));
+const assetFiles = fs.readdirSync(assetPath).filter(file => (file.endsWith('.js') || file.endsWith('.json') && file != "index.js"));
 
 assetFiles.forEach( (file) => {
-	let stuff = Soup.from(require(`./${file}`));
-
-	stuff.forEach( (name, data) => { assets.push(name, data); });
+	var stuff = Soup.from(require(`./${file}`));
+	if (file.endsWith('.js') {
+		stuff.forEach( (name, data) => { assets.push(name, data); });
+	}
+	else if (file.endsWith('.json')) {
+		assets.push(stuff);
+	}
 });
 
 module.exports = assets.pour();
