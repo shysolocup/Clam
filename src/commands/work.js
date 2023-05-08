76877ed92c8d5d5d7cacedd9f99e8d1710f0951d
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-08 10:29:40 -0500

    Update work.js

--- src/commands/work.js
+++ src/commands/work.js
@@ -16,16 +16,16 @@ async function data(ctx, cmd) {
     
     if (!econner.has(ctx.author.id)) new Hand(ctx.author.id);
 
+
     let bal = econner.fetchHand(ctx.author.id);
     let rank = colorify(bal)[1];
+
 	let max = 10*rank;
 	if (bal >= 0) max += (random.int( 25, (bal+50) ));
     let amount = random.int( 25, max );
 
 
 	if ( Catch( bal >= caps.max, { text: "You can't hold anymore." })) return;
-
-
 	if (bal+amount >= caps.max) amount = caps.max-bal;
 
 	
