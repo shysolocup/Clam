bb232a5632650911b57f5928bc67afb93b2aaaa6
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-08 08:06:02 -0500

    Update emojis and emojify.js

--- src/assets/emojis and emojify.js
+++ src/assets/emojis and emojify.js
@@ -1,7 +1,13 @@
 let emojis = {
 	success: "<:confirm:1052011206891798618>",
 	fail: "<:decline:1052011672774131762>",
-	gold: "<:goldenclam:1052759240885940264>"
+	gold: "<:goldenclam:1052759240885940264>",
+	
+	slots: [
+		":fist:",
+		":v:",
+		":raised_hand:"
+	]
 };
 
 function emojify(category) {
