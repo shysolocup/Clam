c292f0ca4e15d9e4d2b1f5ff6a3c0256ff6c6383
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-08 09:55:12 -0500

    Update slots.js

--- src/commands/slots.js
+++ src/commands/slots.js
@@ -1,11 +1,12 @@
 var { psc, bot } = require('../../index.js');
-var { colors, emojis, pearl, pearlify } = require('../assets');
+var { colors, emojis, pearl, pearlify, formatify } = require('../assets');
 
 const { Soup, random } = require('stews');
 
 
 async function data(ctx, cmd) {
-	const { Clan, Catch } = require('../classes');
+	const { Catch, Econner } = require('../classes');
+	let econner = new Econner();
 
 	/* handling */
 	if ( Catch( cmd.onCooldown, { 
@@ -15,30 +16,59 @@ async function data(ctx, cmd) {
 
 	
 	/* emoji stuff */
-	let emojis = [];
+	let rand = [];
 	
 	for (let i = 0; i < 3; i++) {
-		emojis.push(random.choice(emojis.slots));
+		rand.push(random.choice(emojis.slots));
 	}
 	
 	
-	let bet = cmd.args[0];
-	if ( Catch( !bet, { text: "Please put a bet."} )) return;
+	/* handling */
+	let bet = formatify(cmd.args[0]);
+	if (econner.has(ctx.author.id)) var bal = econner.fetchHand(ctx.author.id);
+	if (
+		Catch( !bet, { text: "Please put a bet."} ) ||
+		Catch( !parseInt(bet) && bet.toLowerCase() != "all", { text: "Bet has to be a number or all (`5000`, `5,000`, `5k`)." }) ||
+		Catch( !bal || bal <= 0, { text: "You don't have any money to bet." })
+	) return;
+
 	
+	/* mult and bet formatting */
 	let mult = random.int(2, 10);
+
+	if (bet.toLowerCase() == "all") bet = bal;
+	bet = parseInt(bet);
+
+
+	/* more handling */
+	if (
+		Catch( bet > bal, { text: "You don't have that much." })
+	) return;
 	
 	
+	/* embed stuff */
 	let rawEmbed = {
-		title: "Slot Machine :slot_machine:"
+		title: "Slot Machine :slot_machine:",
+		description: rand.join(" | ")
 	};
 
-	if (emojis[0] == emojis[1] == emojis[2]) {
-		rawEmbed.description = emojis.join(" | ");
-		rawEmbed.footer = `YOU WIN! You`;
+	if (rand[0] == rand[1] && rand[1] == rand[2]) {
+		rawEmbed.footer = `YOU WIN! Your bet was multiplied x${mult}   +${pearl}${pearlify(bet*mult)}`;
+		rawEmbed.color = colors.success;
+
+		econner.addHand(bet*mult, ctx.author.id);
 	}
-	
+	else {
+		rawEmbed.footer = `YOU LOSE! Better luck next time  -${pearl}${pearlify(bet)}`;
+		rawEmbed.color = colors.fail;
+
+		econner.removeHand(bet, ctx.author.id);
+	}
+
+
+	let embed = new psc.Embed(rawEmbed);
 
 	ctx.reply({ embeds: [embed] }).catch(e=>{});
 }
 
-psc.command({ name: "slots", aliases: ["slot"], cooldown: "5s"}, data);
+psc.command({ name: "slots", aliases: ["slot"], cooldown: "10s"}, data);
