2d23d79c946cd0934f8e4da6e5c18fe8e4fba026
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-09 15:11:56 -0500

    Update leaderboard switch.js

--- src/events/leaderboard switch.js
+++ src/events/leaderboard switch.js
@@ -1,1 +1,74 @@
+var { psc, bot } = require('../../index.js');
+var { colors, infostuffs } = require('../assets');
+var { Soup } = require('stews');
 
+async function data(ctx) {
+    var buttonID = ctx.customId;
+
+	if (buttonID.includes("clamLB/switch")) {
+		const { Econner, Catch } = require('../classes');
+        let econner = new Econner();
+
+
+		/* handling */
+		if ( Catch( !infostuffs.has(ctx.message.id), { text: "Command timed out.", poster: ctx.reply.bind(ctx) }) ) return;
+
+
+		/* stuff */
+		var [ user, section, page ] = infostuffs.get(ctx.message.id);
+        page = 0;
+
+
+		/* more handling */
+        if ( Catch( user.id != ctx.member.id, { text: "That's not for you. :angry:", poster: ctx.reply.bind(ctx) }) ) return;
+
+
+        /* getting the leaderboards */
+        var clanLB = await econner.clanLB(ctx.guild.id);
+        var userLB = await econner.userLB(ctx.guild.id, ctx.member.id);
+
+
+        let guild = ctx.guild.name.split("");
+        guild[0] = guild[0].toUpperCase();
+        guild = guild.join("");
+
+
+        section = (section == "user") ? "clan" : "user";
+
+
+        /* button stuff */
+        var disabled = ((section == "user") ? userLB.total : clanLB.total) <= 0;
+
+        let bigLeftButton = new psc.Button({ id: "clamLB/bigLeft", emoji: "⏮️", style: "secondary", disabled: disabled });
+        let leftButton = new psc.Button({ id: "clamLB/left", emoji: "◀", style: "secondary", disabled: disabled });
+        let rightButton = new psc.Button({ id: "clamLB/right", emoji: "▶", style: "secondary", disabled: disabled });
+        let bigRightButton = new psc.Button({ id: "clamLB/bigRight", emoji: "⏭️", style: "secondary", disabled: disabled });
+
+        let switchButton = new psc.Button({ id: `clamLB/switch`, emoji: (section == "user") ? "🛡️" : "👥", style: "secondary" });
+        
+        let row = new psc.ActionRow([ bigLeftButton, leftButton, switchButton, rightButton, bigRightButton ]);
+
+
+        /* embed stuff */
+        let embed = new psc.Embed({
+            author: { name: `${guild} Leaderboards` },
+
+            title: (section == "user") ? "Users Leaderboard" : "Clans Leaderboard",
+            description: (section == "user") ? 
+                ((userLB.total <= 0) ? "None" : `${userLB.content[page].join("\n")}\n** **`) : 
+                ((clanLB.total <= 0) ? "None" : `${clanLB.content[page].join("\n")}\n** **`) ,
+
+            footer: `Page ${page+1}/${ (section == "user") ? userLB.pages : clanLB.pages }`,
+
+            thumbnail: ctx.guild.iconURL(),
+            color: colors.clam
+        });
+
+
+        ctx.update({ embeds: [embed], components: [row] }).catch(e=>{});
+
+        infostuffs.set(ctx.message.id, [user, section, page]);
+    }
+}
+
+psc.buttonAction(data);
