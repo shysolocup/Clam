3db2135f7f72f36532724a7edf52226700c59627
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-09 17:48:11 -0500

    Fixed an issue where cooldown messages wouldn't delete like they should

--- src/commands/work.js
+++ src/commands/work.js
@@ -10,7 +10,7 @@ async function data(ctx, cmd) {
 	
 	if ( Catch( cmd.onCooldown, {
 		head: `Woah there!  :face_with_spiral_eyes:`,
-		text: `You've been timed out from using this command for a bit.`,
+		text: `You've been timed out from using this command for a bit.`
 	}) ) return;
 
     
