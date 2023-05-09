d38c9ef6e1fb949b64b84a61afee1b1c2aca905c
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-09 17:45:26 -0500

    Update invite.js

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -6,7 +6,7 @@ var { Clanner, Catch } = require('../classes');
 async function data(ctx, cmd) {
 	if ( Catch( cmd.onCooldown, { 
 		head: `Woah there!  :face_with_spiral_eyes:`,
-		text: `You've been timed out from using this command for a bit.`,
+		text: `You've been timed out from using this command for a bit.`
 	}) ) return;
 	
 
