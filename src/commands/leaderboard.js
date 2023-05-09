8bf0739644e0bc2e7f18f8d21e714cdf214abc85
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-09 14:53:45 -0500

    Update leaderboard.js

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -7,7 +7,7 @@ async function data(ctx, cmd) {
 	let econner = new Econner();
 
 	let page = 0;
-	let section = "clan";
+	let section = "user";
 
 
 	/* getting the leaderboards */
@@ -20,14 +20,27 @@ async function data(ctx, cmd) {
 	guild = guild.join("");
 
 
+	/* button stuff */
+	var disabled = ((section == "user") ? userLB.total : clanLB.total) <= 0;
+
+	let bigLeftButton = new psc.Button({ id: "clamLB/bigLeft", emoji: "⏮️", style: "secondary", disabled: disabled });
+	let leftButton = new psc.Button({ id: "clamLB/left", emoji: "◀", style: "secondary", disabled: disabled });
+	let rightButton = new psc.Button({ id: "clamLB/right", emoji: "▶", style: "secondary", disabled: disabled });
+    let bigRightButton = new psc.Button({ id: "clamLB/bigRight", emoji: "⏭️", style: "secondary", disabled: disabled });
+
+	let switchButton = new psc.Button({ id: `clamLB/switch`, emoji: (section == "user") ? "🛡️" : "👥", style: "secondary" });
+	
+	let row = new psc.ActionRow([ bigLeftButton, leftButton, switchButton, rightButton, bigRightButton ]);
+
+
 	/* embed stuff */
 	let embed = new psc.Embed({
 		author: { name: `${guild} Leaderboards` },
 
 		title: (section == "user") ? "Users Leaderboard" : "Clans Leaderboard",
 		description: (section == "user") ? 
-			((userLB.count <= 0) ? "None" : `${userLB.content[page].join("\n")}\n** **`) : 
-			((clanLB.count <= 0) ? "None" : `${clanLB.content[page].join("\n")}\n** **`) ,
+			((userLB.total <= 0) ? "None" : `${userLB.content[page].join("\n")}\n** **`) : 
+			((clanLB.total <= 0) ? "None" : `${clanLB.content[page].join("\n")}\n** **`) ,
 
 		footer: `Page ${page+1}/${ (section == "user") ? userLB.pages : clanLB.pages }`,
 
@@ -36,9 +49,10 @@ async function data(ctx, cmd) {
 	});
 
 
-	let a = ctx.reply({ embeds: [embed], components: [] }).catch(e=>{});
+	let a = ctx.reply({ embeds: [embed], components: [row] }).catch(e=>{});
 
-	infostuffs.push(a.id, [ctx.author, page]);
+	
+	infostuffs.push(a.id, [ctx.author, section, page]);
 	setTimeout(() => infostuffs.delete(a.id), 21600000);
 }
 
