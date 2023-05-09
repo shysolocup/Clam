6f684bc305bf2a53d24e4132ed6a162de047aea6
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-09 14:38:25 -0500

    Update leaderboard.js

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -1,58 +1,18 @@
 var { psc, bot } = require('../../index.js');
-var { colors, colorify, pearl, pearlify, emojis } = require('../assets');
+var { colors, infostuffs } = require('../assets');
 var { Soup } = require('stews');
 
 async function data(ctx, cmd) {
 	const { Econner, Catch } = require('../classes');
 	let econner = new Econner();
 
+	let page = 0;
+	let section = "clan";
+
 
 	/* getting the leaderboards */
-	var clanLB = Soup.from(await econner.clanLB(ctx.guild.id));
-	var userLB = Soup.from(await econner.userLB(ctx.guild.id));
-
-
-	/* fixing the leaderboards */
-	clanLB = clanLB.mapKey( (k, v) => {
-		let stuff = k.split("/");
-		stuff.shift();
-		return stuff.join("");
-	});
-
-
-	let userRanks = new Soup(Object);
-	userLB.forEach( (k, v, i) => {
-		let rank = `${i+1}${((int) => {
-			if (int > 3 && int < 21) return "th";
-			switch( int % 10) {
-				case 1: return "st"; break;
-				case 2: return "nd"; break;
-				case 3: return "rd"; break;
-				default: return "th"; break;
-			}
-		})(i+1)}`;
-
-		userRanks.push(k, rank);
-	});
-	
-
-	let clanRanks = new Soup(Object);
-	clanLB.forEach( (k, v, i) => {
-		let rank = `${i+1}${((int) => {
-			if (int > 3 && int < 21) return "th";
-			switch( int % 10) {
-				case 1: return "st"; break;
-				case 2: return "nd"; break;
-				case 3: return "rd"; break;
-				default: return "th"; break;
-			}
-		})(i+1)}`;
-
-		clanRanks.push(k, rank);
-	});
-
-
-	var userRank = (userRanks.includes(ctx.author.id)) ? userRanks.get(ctx.author.id) : null;
+	var clanLB = await econner.clanLB(ctx.guild.id);
+	var userLB = await econner.userLB(ctx.guild.id);
 
 
 	let guild = ctx.guild.name.split("");
@@ -60,63 +20,26 @@ async function data(ctx, cmd) {
 	guild = guild.join("");
 
 
-	/* setup for embed fields and stuff */
-	let userFields = new Soup(Array);
-
-	userRanks.forEach( (k, v, i) => {
-		let stuff = `${ (k == ctx.author.id) ? `**${v}**` : `${v}` }:**  **<@${k}> \n ${"`"+pearl}${pearlify(userLB.get(i))+"`"}`;
-		userFields.push(stuff);
-	});
-
-
-	if (userFields.length > 5) {
-		userFields.scoop( (_, i) => {
-			return i >= 5;
-		});
-		userFields.push("\n...\n");
-		let rankInt = userRanks.indexOf(ctx.author.id)+1;
-		if (rankInt > 5) {
-			if (userRanks.get(rankInt-2)) userFields.push(`${userRanks.get(rankInt-2)}:**  **<@${userRanks.keys[rankInt-2]}> \n ${"`"+pearl}${pearlify(userLB.get(rankInt-2))+"`"}`);
-
-			userFields.push(`**${userRank}**:**  **<@${ctx.author.id}> \n ${"`"+pearl}${pearlify(userLB.get(rankInt-1))+"`"}`);
-
-			if (userRanks.get(rankInt)) userFields.push(`${userRanks.get(rankInt)}:**  **<@${userRanks.keys[rankInt]}> \n ${"`"+pearl}${pearlify(userLB.get(rankInt))+"`"}`);
-		}
-	}
-
-
-	let clanFields = new Soup(Array);
-
-	clanRanks.forEach( (k, v, i) => {
-		let clan = clanLB.get(i);
-		let stuff = `${v}:**  **${clan.name} (${"`"+clan.id+"`"}) \n ${"`"+pearl}${pearlify(clan.funds)+"`"}`;
-		clanFields.push(stuff);
-	});
-
-	if (clanFields.length > 5) {
-		clanFields.scoop( (_, i) => {
-			return i >= 5;
-		});
-		clanFields.push("\n...\n");
-	}
-
-
 	/* embed stuff */
 	let embed = new psc.Embed({
-		title: `${guild} Leaderboard`,
-		description: (userRank) ? `You are ranked **${userRank}**.` : "You aren't on the leaderboard yet.",
+		author: { name: `${guild} Leaderboards` },
 
-		fields: [
-			{ name: "Users", value: (userFields.join("")=="") ? "None" : userFields.join("\n"), inline: true },
-			{ name: "** **", value: "** **", inline: true },
-			{ name: "Clans", value: (clanFields.join("")=="") ? "None" : clanFields.join("\n"), inline: true }
-		],
+		title: (section == "user") ? "Users Leaderboard" : "Clans Leaderboard",
+		description: (section == "user") ? 
+			((userLB.count <= 0) ? "None" : `${userLB.content[page].join("\n")}\n** **`) : 
+			((clanLB.count <= 0) ? "None" : `${clanLB.content[page].join("\n")}\n** **`) ,
 
-		color: colors.clam,
-		thumbnail: ctx.guild.iconURL()
+		footer: `Page ${page+1}/${ (section == "user") ? userLB.pages : clanLB.pages }`,
+
+		thumbnail: ctx.guild.iconURL(),
+		color: colors.clam
 	});
 
-	ctx.reply({ embeds: [embed] });
+
+	let a = ctx.reply({ embeds: [embed], components: [] }).catch(e=>{});
+
+	infostuffs.push(a.id, [ctx.author, page]);
+	setTimeout(() => infostuffs.delete(a.id), 21600000);
 }
 
 psc.command({name: "leaderboard", aliases: ["lb"]}, data);
