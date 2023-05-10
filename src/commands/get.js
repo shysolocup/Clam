8571eb9a40b55c62b69df0b080715460c9601670
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-10 10:05:20 -0500

    Update command get
    added cooldown message

--- src/commands/get.js
+++ src/commands/get.js
@@ -5,6 +5,13 @@ var { Clanner, Catch } = require('../classes');
 const { Soup } = require('stews');
 
 async function data(ctx, cmd) {
+	if ( Catch( cmd.onCooldown, {
+		head: `Woah there!  :face_with_spiral_eyes:`,
+		text: `You can use this command again ${ cmd.cooldown.relative }`,
+		time: cmd.cooldown.time
+	}) ) return;
+
+
 	let clans = new Clanner();
 	let id = cmd.args[0];
 	
