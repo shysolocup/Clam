6c1e8b88f6282faa193893a2c331da074cef3a2a
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-10 10:09:15 -0500

    Update command give
    added cooldown message

--- src/commands/give.js
+++ src/commands/give.js
@@ -14,9 +14,10 @@ async function data(ctx, cmd) {
     
     if ( 
         Catch( cmd.onCooldown, {
-		    head: `Woah there!  :face_with_spiral_eyes:`,
-		    text: `You've been timed out from using this command for a bit.`
-	    }) ||
+			head: `Woah there!  :face_with_spiral_eyes:`,
+			text: `You can use this command again ${ cmd.cooldown.relative }`,
+			time: cmd.cooldown.time
+		}) ||
 
         Catch( !user, { text: `Please put a valid user.` }) ||
         Catch( !econner.has(ctx.author.id) || econner.fetchHand(ctx.author.id) <= 0, { text: "You don't have any pearls to give." }) ||
