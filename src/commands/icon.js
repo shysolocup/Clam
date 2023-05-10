b80239b99589704a765adc43ae161508ff9f0550
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-10 10:14:00 -0500

    Update command icon
    - added cooldown message
    - added catch to prevent spam crashing

--- src/commands/icon.js
+++ src/commands/icon.js
@@ -5,6 +5,12 @@ var { Clanner, Catch } = require('../classes');
 const { Soup } = require('stews');
 
 async function data(ctx, cmd) {
+	if ( Catch( cmd.onCooldown, { 
+		head: `Woah there!  :face_with_spiral_eyes:`,
+		text: `You can use this command again ${ cmd.cooldown.relative }`,
+		time: cmd.cooldown.time
+	}) ) return;
+
 	let clans = new Clanner();
 	let id = cmd.args[0];
 	
@@ -33,7 +39,7 @@ async function data(ctx, cmd) {
     ctx.reply({
         content: `Here's the icon for ${clan.name} (${"`"+id+"`"}):`,
         files: [{ attachment: icon, name: `clan-icon.png` }]
-    })
+    }).catch(e=>{});
 }
 
 psc.command("icon", data);
