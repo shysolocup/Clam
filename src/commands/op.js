1f4bb2886b75c4437d9880ad78026447e66dd448
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-10 10:30:21 -0500

    Update command op
    updated cooldown message

--- src/commands/op.js
+++ src/commands/op.js
@@ -9,7 +9,8 @@ async function data(ctx, cmd) {
 	
 	if ( Catch( cmd.onCooldown, { 
 		head: `Woah there!  :face_with_spiral_eyes:`,
-		text: `You've been timed out from using this command for a bit.`
+		text: `You can use this command again ${ cmd.cooldown.relative }`,
+		time: cmd.cooldown.time
 	}) ) return;
 
 	
@@ -49,5 +50,6 @@ async function data(ctx, cmd) {
 	ctx.reply({ embeds: [embed] }).catch(e=>{});
 	clans.op(clan.id, user.id, ctx.guild.id);
 }
+
 
 psc.command({ name: "op", cooldown: "2s"}, data);
