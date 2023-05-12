2bf00d92bc0cb85c03d2b68d933c9736b2ab279a
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-12 12:32:55 -0500

    Create part force disband

---
+++ src/commands/insides/force/parts/disband.js
@@ -0,0 +1,55 @@
+var { psc, bot } = require('../../../../../index.js');
+var { colors, emojis, infostuffs } = require('../../../../assets');
+var { Clanner, Catch } = require('../../../../classes');
+
+const { Soup } = require('stews');
+
+
+async function data(ctx, cmd) {
+	let clans = new Clanner();
+	let id = cmd.args[0];
+    
+    /* handling */
+    if (
+        Catch( cmd.onCooldown, { 
+			head: `Woah there!  :face_with_spiral_eyes:`,
+			text: `You can use this command again ${ cmd.cooldown.relative }`,
+			time: cmd.cooldown.time
+		}) ||
+
+    	Catch( !id, { text: 'Please put a clan ID.' }) ||
+        Catch( !clans.has(id, ctx.guild.id), { text: "There is no clan with that ID." })
+    ) return;
+
+
+	var clan = clans.fetch(id);
+	
+    
+    /* buttons n stuff */
+    let disbandAccept = new psc.Button({ id: "disbandAccept", label: "Yes", style: "success"});
+    let disbandDecline = new psc.Button({ id: "disbandDecline", label: "No", style: "danger"});
+
+    let row = new psc.ActionRow([disbandAccept, disbandDecline]);
+
+
+    /* embed stuff */
+    let embed = new psc.Embed({
+        title: `Clan Disbanding  🗑️`,
+        description: `Are you sure you would like to disband ${"`"+clan.name+"`"}`,
+
+        footer: `( id: ${clan.id} )`,
+
+        color: colors.blurple
+    });
+
+
+	let a = await ctx.reply({ embeds: [embed], components: [row] }).catch(e=>{});
+
+    infostuffs.push(a.id, [clan, ctx.author.id] );
+    setTimeout(() => infostuffs.delete(a.id), 21600000);
+}
+
+module.exports = {
+    name: "disband",
+    data: data
+}
