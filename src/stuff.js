00261ac611a31ea0f5a818835917a6d5e3335dad
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-12 11:48:54 -0500

    Update stuff.js

--- src/stuff.js
+++ src/stuff.js
@@ -13,4 +13,23 @@ class ID {
     }
 }
 
-module.exports = { ID };
+
+function vary(thing, obj) {
+    var stuff = [];
+	if (obj[thing]) {
+        stuff.push( obj[thing] );
+	}
+    else if (obj[thing.constructor.name]) {
+        stuff.push( obj[thing.constructor.name] );
+    }
+    else if (obj[typeof thing]) {
+        stuff.push( obj[typeof thing] );
+    }
+
+    stuff.push(thing);
+
+    return (stuff[0] instanceof Function) ? stuff[0].bind(stuff[1])() : stuff[0];
+}
+
+
+module.exports = { ID, vary };
