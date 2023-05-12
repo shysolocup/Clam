def9123cdb907c8f1cbbdd0b3ee6583559ec0d8a
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-12 12:28:14 -0500

    Create insides force

--- src/commands/insides/force/index.js
+++ src/commands/insides/force/index.js
@@ -1,1 +1,27 @@
+var { psc, bot } = require('../../../../index.js');
+var { colors, emojis } = require('../../../assets/index.js');
+var { Clanner, Catch } = require('../../../classes/index.js');
+var { vary } = require('../../../stuff.js');
+const { Soup } = require('stews');
 
+var parts = require('./parts');
+
+async function data(ctx, cmd) {
+	let disabled = !(psc.author.hasPermissions(["admin"]) || isDev(ctx.author.id));
+    if ( Catch( disabled, { post: false }) ) return;
+
+
+    let part = cmd.args[0];
+    cmd.args.shift();
+
+    if ( 
+        Catch( !part, { text: "Please put a command to force." }) ||
+        Catch( !parts.parts.includes(part), { text: "Please put a valid command to force" })
+    ) return;
+
+
+    return parts[part](ctx, cmd);
+}
+
+
+module.exports = { data };
