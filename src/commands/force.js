fd582bcdf07e59ba4ac29ceb01f14d5c9e3bb756
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-12 11:26:24 -0500

    Create command force

--- src/commands/force.js
+++ src/commands/force.js
@@ -1,1 +1,8 @@
+/* THIS IS A DEV/ADMIN ONLY COMMAND AND THUS ONLY SHOWS UP IN THE LOCKED ADMINISTRATOR SECTION */
+/* IT ALSO USES INSIDES SO THAT IT CAN BE BETTER ORGANIZED BECAUSE IT COVERS MULTIPLE COMMANDS IN ONE */
 
+var { psc, bot } = require('../../index.js');
+var { data } = require('./insides/force');
+
+
+psc.command("force", data);
