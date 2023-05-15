f47ab80cadc96f04d3c13159d301e03eccca8802
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-15 08:28:44 -0500

    Create class Inventory

---
+++ src/classes/Inventory.js
@@ -0,0 +1,20 @@
+const { Soup } = require('stews');
+
+class Inventory {
+    constructor(ctx, userID) {
+        var inv = Soup.from(require('../data/inventories.json'));
+
+        if (!inv.has(ctx.guild.id)) inv.push(ctx.guild.id, new Soup(Object));
+        var parent = Soup.from(inv.get(ctx.guild.id));
+
+        if (!parent.has(userID)) parent.push(userID, []);
+        
+        inv.dump('./src/data/inventories.json', null, 4);
+
+        
+        return this;
+    }
+}
+
+
+module.exports = { Inventory };
