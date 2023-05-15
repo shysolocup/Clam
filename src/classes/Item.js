80394da9dfbb733fa6bf4c85629ad9654139349c
nutmeg <88659700+nuttmegg@users.noreply.github.com>
2023-05-15 09:18:04 -0500

    Update class Item

--- src/classes/Item.js
+++ src/classes/Item.js
@@ -3,19 +3,19 @@ const { ItemTemplate } = require('./ItemTemplate.js');
 
 class Item {
     constructor(ctx, clanID, name=null) {
-        var items = Soup.from(require('../data/items.json'));
+        var items = Soup.from(require('../data/shops.json'));
 
         var contents = new ItemTemplate(ctx, clanID, name);
 
         if (!items.has(ctx.guild.id)) items.push(ctx.guild.id, new Soup(Object));
         var parentGuild = Soup.from(items.get(ctx.guild.id));
 	    
-	if (!parentGuild.has(clanID)) parentGuild.push(clanID, new Soup(Object));
-	var parent = Soup.from(parentGuild.get(clanID));
+	    if (!parentGuild.has(clanID)) parentGuild.push(clanID, new Soup(Object));
+	    var parent = Soup.from(parentGuild.get(clanID));
 
         if (!parent.has(contents.name)) parent.push(contents.name, contents),
         
-        items.dump('./src/data/items.json', null, 4);
+        items.dump('./src/data/shops.json', null, 4);
 
         contents.forEach( (key, value) => {
             this[key] = value;
